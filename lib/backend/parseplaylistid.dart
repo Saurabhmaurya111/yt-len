8cbc0d18ca68d3e67e045ac624cece7ac7b4e710
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2024-06-10 11:22:01 +0530

    Added all the function in get len

--- lib/backend/parseplaylistid.dart
+++
@@ -1,9 +0,0 @@
-
-
-  String? parsePlaylistId(String url) {
-    final uri = Uri.tryParse(url);
-    if (uri == null || !uri.queryParameters.containsKey('list')) {
-      return null;
-    }   
-    return uri.queryParameters['list'];
-  }
--- lib/get_len.dart
+++ lib/get_len.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:last_moment/backend/fetch_Playlist_Items.dart';
-import 'package:last_moment/backend/fetch_total_duration.dart';
-import 'package:last_moment/backend/fomat_duration.dart';
-import 'package:last_moment/backend/parseplaylistid.dart';
+// import 'package:last_moment/backend/fetch_Playlist_Items.dart';
+// import 'package:last_moment/backend/fetch_total_duration.dart';
+// import 'package:last_moment/backend/fomat_duration.dart';
+// import 'package:last_moment/backend/parseplaylistid.dart';
 import 'package:last_moment/components/appbar.dart';
 import 'package:last_moment/components/searchbar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class GetLength extends StatefulWidget {
   const GetLength({super.key});
@@ -14,46 +16,114 @@ class GetLength extends StatefulWidget {
 }
 
 class _GetLengthState extends State<GetLength> {
-  final TextEditingController _controller = TextEditingController();
-  bool _loading = false;
-  String _totalDuration = '';
+ final TextEditingController _controller = TextEditingController();
+  final String apiKey = 'Your Api Key'; // Replace with your API key
+  String displayText = '';
 
-  Future<void> fetchPlaylistDuration(String playlistUrl) async {
-    setState(() {
-      _loading = true;
-      _totalDuration = '';
-    });
-    print('Fetching playlist duration for URL: $playlistUrl');
-
-    final playlistId = parsePlaylistId(playlistUrl);
-    print('Parsed playlist ID: $playlistId');
-
-    if (playlistId == null) {
+  Future<void> fetchPlaylistLength(String playlistLink) async {
+    final playlistId = getId(playlistLink);
+    if (playlistId == 'invalid_playlist_link') {
       setState(() {
-        _loading = false;
-        _totalDuration = 'Invalid playlist URL';
+        displayText = 'Invalid playlist link';
       });
       return;
     }
-    try {
-      final videoIds = await fetchPlaylistItems(playlistId);
-      print('Fetched video IDs: $videoIds');
-      final totalDuration = await fetchTotalDuration(videoIds);
-      print('Fetched total duration: $totalDuration');
-      setState(() {
-        _totalDuration = formatDuration(totalDuration);
-        print('Formatted duration: $_totalDuration');
-      });
-    } catch (error) {
-      print('Error: $error');
-      setState(() {
-        _totalDuration = 'Error fetching playlist duration: $error';
-      });
-    } finally {
-      setState(() {
-        _loading = false;
-      });
+
+    final url1 = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&fields=items/contentDetails/videoId,nextPageToken&key=$apiKey&playlistId=$playlistId&pageToken=';
+    final url2 = 'https://www.googleapis.com/youtube/v3/videos?&part=contentDetails&id={}&key=$apiKey&fields=items/contentDetails/duration';
+    Duration totalDuration = Duration();
+    int videoCount = 0;
+    String nextPageToken = '';
+
+    while (true) {
+      final response1 = await http.get(Uri.parse(url1 + nextPageToken));
+      final results1 = json.decode(response1.body);
+      if (results1.containsKey('error')) {
+        setState(() {
+          displayText = results1['error']['message'];
+        });
+        return;
+      }
+
+      final videoIds = (results1['items'] as List)
+          .map((item) => item['contentDetails']['videoId'])
+          .toList();
+      videoCount += videoIds.length;
+
+      final response2 = await http.get(Uri.parse(url2.replaceFirst('{}', videoIds.join(','))));
+      final results2 = json.decode(response2.body);
+      if (results2.containsKey('error')) {
+        setState(() {
+          displayText = results2['error']['message'];
+        });
+        return;
+      }
+
+      for (var item in results2['items']) {
+        final duration = parseDuration(item['contentDetails']['duration']);
+        totalDuration += duration;
+      }
+
+      if (results1.containsKey('nextPageToken') && videoCount < 500) {
+        nextPageToken = results1['nextPageToken'];
+      } else {
+        break;
+      }
     }
+
+    setState(() {
+      displayText = 'No of videos : $videoCount\n'
+          'Average length of video : ${formatDuration((totalDuration.inSeconds / videoCount).round())}\n'
+          'Total length of playlist : ${formatDuration(totalDuration.inSeconds)}\n'
+          'At 1.25x : ${formatDuration((totalDuration.inSeconds / 1.25).round())}\n'
+          'At 1.50x : ${formatDuration((totalDuration.inSeconds / 1.5).round())}\n'
+          'At 1.75x : ${formatDuration((totalDuration.inSeconds / 1.75).round())}\n'
+          'At 2.00x : ${formatDuration((totalDuration.inSeconds / 2).round())}';
+    });
+  }
+
+  String getId(String playlistLink) {
+    final regex = RegExp(r'^([\S]+list=)?([\w_-]+)[\S]*$');
+    final match = regex.firstMatch(playlistLink);
+    return match != null ? match.group(2)! : 'invalid_playlist_link';
+  }
+
+  Duration parseDuration(String isoString) {
+    final regex = RegExp(r'PT(\d+H)?(\d+M)?(\d+S)?');
+    final match = regex.firstMatch(isoString);
+
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+
+    if (match != null) {
+      if (match.group(1) != null) {
+        hours = int.parse(match.group(1)!.replaceAll('H', ''));
+      }
+      if (match.group(2) != null) {
+        minutes = int.parse(match.group(2)!.replaceAll('M', ''));
+      }
+      if (match.group(3) != null) {
+        seconds = int.parse(match.group(3)!.replaceAll('S', ''));
+      }
+    }
+
+    return Duration(hours: hours, minutes: minutes, seconds: seconds);
+  }
+
+  String formatDuration(int totalSeconds) {
+    final duration = Duration(seconds: totalSeconds);
+    final days = duration.inDays;
+    final hours = duration.inHours.remainder(24);
+    final minutes = duration.inMinutes.remainder(60);
+    final seconds = duration.inSeconds.remainder(60);
+
+    final dayStr = days > 0 ? '$days day${days > 1 ? 's' : ''}, ' : '';
+    final hourStr = hours > 0 ? '$hours hour${hours > 1 ? 's' : ''}, ' : '';
+    final minuteStr = minutes > 0 ? '$minutes minute${minutes > 1 ? 's' : ''}, ' : '';
+    final secondStr = '$seconds second${seconds != 1 ? 's' : ''}';
+
+    return '$dayStr$hourStr$minuteStr$secondStr'.replaceFirst(RegExp(r', $'), '');
   }
 
   @override
@@ -79,7 +149,7 @@ class _GetLengthState extends State<GetLength> {
             ),
             Center(
               child: CusotomSearchBar(
-                 controller: _controller,
+                controller: _controller,
                 hint: 'youtube.com/playlist?list=ID',
               ),
             ),
@@ -93,36 +163,40 @@ class _GetLengthState extends State<GetLength> {
               onPressed: () {
                 final playlistUrl = _controller.text;
                 if (playlistUrl.isNotEmpty) {
-                  fetchPlaylistDuration(playlistUrl);
+                  fetchPlaylistLength(_controller.text);
                 }
               },
               child: Text(
                 'Analyze',
-                style: TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.w400),
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
               ),
             ),
-            
-            if (_loading)
-              CircularProgressIndicator(
-                color: Colors.white,
-              )
-            else
-              Text(
-                _totalDuration,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
+            // if (_loading)
+            //   CircularProgressIndicator(
+            //     color: Colors.white,
+            //   )
+            // else
+            //   Text(
+            //     _totalDuration,
+            //     style: TextStyle(
+            //         fontSize: 24,
+            //         fontWeight: FontWeight.bold,
+            //         color: Colors.white),
+            //   ),
+            SizedBox(height: 10,),
+             Text(displayText , style: TextStyle(color: Colors.white),),
             SizedBox(
               height: 10,
             ),
-            Text(
-              "Disclaimer",
-              style: TextStyle(fontSize: 15, color: Colors.white),
-            ),
-            Text(
-              "You can enter a playlist link, playlist ID or even a video link from the playlist! ",
-              style: TextStyle(color: Colors.white, fontSize: 10),
-            ),
+            // Text(
+            //   "Disclaimer",
+            //   style: TextStyle(fontSize: 15, color: Colors.white),
+            // ),
+            // Text(
+            //   "You can enter a playlist link, playlist ID or even a video link from the playlist! ",
+            //   style: TextStyle(color: Colors.white, fontSize: 10),
+            // ),
             Spacer(),
             Text(
               "Made With ❤️ by Saurabh Maurya",
